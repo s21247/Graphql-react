d0fa9d7d5ad835ffebc9bb9821b9baa244faa9d6
s21247 <s21247@pjwstk.edu.pl>
2022-09-13 23:09:44 +0200

    refactoring code into smaller components and reusable

--- src/Components/Layout/Shipping.tsx
+++ src/Components/Layout/Shipping.tsx
@@ -5,8 +5,8 @@ import ChangeAddress from "./inputsShipping/ChangeAddress";
 import {countTotalPrice} from "../Helpers/cartHelpers";
 import RadioInput from "./inputsShipping/RadioInput";
 import {useNavigate} from "react-router-dom";
-import checkout from "../Features/checkout/Checkout";
 import {checkoutPage} from "../Routing/Routing";
+import Subtotal from "./Subtotal";
 
 const Shipping = () => {
     const [isOpen,setIsOpen] = useState<boolean>(false)
@@ -49,67 +49,38 @@ const Shipping = () => {
         e.preventDefault()
         //dispatch here
         console.log(JSON.stringify(shipping))
-        navigate(checkoutPage)
     }
     const {country,...otherProps} = shipping
 
     const canSave = [...Object.values(otherProps)].every(Boolean)
 
     return (
+        <>
         <form onSubmit={handleSubmit} >
         <div className="border border-gray-300 items-center w-full font-semibold">
-            <div className="flex flex-row w-full py-5 ">
-            <h1 className="ml-5">Subtotal</h1>
-            <p className="ml-auto mr-5">${totalPrice}.00</p>
-            </div>
-            <div className="border-t-2 border-gray-300 items-center w-full flex-col px-5 pt-5">
-                <p>Shipping</p>
-
-                <RadioInput
-                    handleChange={handleChange}
-                    name={'value'}
-                    value={standard}
-                    id={"Standard"}
-                    checkVal={shipping.value}
-                    inputName={'standard'}
-                />
-                <RadioInput
-                    name={'value'}
-                    handleChange={handleChange}
-                    id={"Express"}
-                    checkVal={shipping.value}
-                    value={express}
-                    inputName={'express'}
-                />
-                <p className="pt-5">Shipping to <span className="font-bold">{shipping.town}</span></p>
-                <button type={"button"} className="flex flex-row w-full py-5" onClick={handleOnClick}>
-                <svg className="svg-icon" viewBox="0 0 20 20" width="20" height="20">
-                    <path d="M10,1.375c-3.17,0-5.75,2.548-5.75,5.682c0,6.685,5.259,11.276,5.483,11.469c0.152,0.132,0.382,0.132,0.534,0c0.224-0.193,5.481-4.784,5.483-11.469C15.75,3.923,13.171,1.375,10,1.375 M10,17.653c-1.064-1.024-4.929-5.127-4.929-10.596c0-2.68,2.212-4.861,4.929-4.861s4.929,2.181,4.929,4.861C14.927,12.518,11.063,16.627,10,17.653 M10,3.839c-1.815,0-3.286,1.47-3.286,3.286s1.47,3.286,3.286,3.286s3.286-1.47,3.286-3.286S11.815,3.839,10,3.839 M10,9.589c-1.359,0-2.464-1.105-2.464-2.464S8.641,4.661,10,4.661s2.464,1.105,2.464,2.464S11.359,9.589,10,9.589"/>
-                </svg>
-                    <p className="pl-2">Change address</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                         className="bi bi-chevron-down my-auto pl-2" viewBox="0 0 16 16">
-                        <path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                    </svg>
-                </button>
-                <ChangeAddress
-                    open={isOpen}
-                    handleChange={handleChange}
-                    shipping={shipping}
-                    selectChange={selectChange}
-                    canSave={canSave}
-                />
-            </div>
+            <Subtotal
+                display={true}
+                onClick={handleOnClick}
+                open={isOpen}
+                handleChange={handleChange}
+                shipping={shipping}
+                selectChange={selectChange}
+                canSave={canSave} standard={standard}
+                express={express} totalPrice={totalPrice}
+            />
             <div className="flex flex-row w-full py-5 border-t-2 border-gray-300 items-center px-5">
                 <p>Total</p>
                 <p className="ml-auto">${totalPrice + parseInt(shipping.value)}.00</p>
 
             </div>
         </div>
-            <button className="bg-black w-full items-center text-white py-4 mt-4 hover:bg-gray-800">
-                Proceed to checkout
-            </button>
         </form>
+    <button
+        onClick={() => navigate(checkoutPage)}
+        className="bg-black w-full items-center text-white py-4 mt-4 hover:bg-gray-800">
+        Proceed to checkout
+    </button>
+    </>
     );
 };
 
--- src/Components/Layout/inputsShipping/ChangeAddress.tsx
+++ src/Components/Layout/inputsShipping/ChangeAddress.tsx
@@ -1,49 +1,53 @@
-import React from 'react';
+import React from "react";
 import SelectCountry from "./SelectCountry";
 
 type props = {
-    open: boolean
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-    shipping: {value: string,town: "California",zip:"",country: "Usa"}
-    selectChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
-    canSave: Boolean
-
-}
-const ChangeAddress = ({open,handleChange,shipping, selectChange,canSave}: props) => {
-    if(!open) return null
-    return (
-        <div className="flex flex-col w-full items-center mb-4">
-            <SelectCountry
-                selectChange={selectChange}
-            shipping={shipping}
-            />
-            <input
-                className="border border-gray-300 outline-0 w-full py-4 pl-3"
-                placeholder={"Town / City"}
-                type={"text"}
-                id={"town"}
-                name={"town"}
-                onChange={handleChange}
-                value={shipping.town}
-                pattern="([A-Z])[\w+.]{1,}"
-            />
-            <input
-                className="border border-gray-300 outline-0 w-full py-4 pl-3 mt-2"
-                placeholder={"Zip code"}
-                id={"zip"}
-                name={"zip"}
-                type={"text"}
-                value={shipping.zip}
-                onChange={handleChange}
-                pattern="[0-9]{5}"
-                maxLength={5}
-            />
-            <button disabled={!canSave} className="py-4 w-full bg-black text-white mt-2 hover:bg-gray-800">
-                Save Changes
-            </button>
-
-        </div>
-    );
+  open: boolean;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  shipping: { value: string; town: "California"; zip: ""; country: "Usa" };
+  selectChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  canSave: Boolean;
+};
+const ChangeAddress = ({
+  open,
+  handleChange,
+  shipping,
+  selectChange,
+  canSave,
+}: props) => {
+  if (!open) return null;
+  return (
+    <div className="flex flex-col w-full items-center mb-4">
+      <SelectCountry selectChange={selectChange} shipping={shipping} />
+      <input
+        className="border border-gray-300 outline-0 w-full py-4 pl-3"
+        placeholder={"Town / City"}
+        type={"text"}
+        id={"town"}
+        name={"town"}
+        onChange={handleChange}
+        value={shipping.town}
+        pattern="([A-Z])[\w+.]{1,}"
+      />
+      <input
+        className="border border-gray-300 outline-0 w-full py-4 pl-3 mt-2"
+        placeholder={"Zip code"}
+        id={"zip"}
+        name={"zip"}
+        type={"text"}
+        value={shipping.zip}
+        onChange={handleChange}
+        pattern="[0-9]{5}"
+        maxLength={5}
+      />
+      <button
+        disabled={!canSave}
+        className="py-4 w-full bg-black text-white mt-2 hover:bg-gray-800"
+      >
+        Save Changes
+      </button>
+    </div>
+  );
 };
 
 export default ChangeAddress;
